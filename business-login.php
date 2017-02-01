b84ce70bc0809c8d8e9782033fbe2e5103c97a85
mjr6d9 <mjr6d9@mail.missouri.edu>
2017-02-01 17:34:57 -0600

    Added basic login and home web pages for business and user

---
+++ business-login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+ 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Business Login</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<style type="text/css">
+			.jumbotron{
+				height: 100vh;
+			}
+
+			.jumbotron .container {
+ 				max-width: 100%;
+			}
+
+			.row{
+				margin: auto;
+			}	
+		</style>	
+	</head>
+	<body>
+		<div class="jumbotron">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-6">
+						<h1>Business Login Page</h1>
+						<div class="login-group">
+							<div class="form-group">
+								<label for="lg_username" class="sr-only">Username</label>
+								<input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="Username">
+							</div>
+							<div class="form-group">
+								<label for="lg_password" class="sr-only">Password</label>
+								<input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="Password">
+							</div>
+							<div class="form-group login-group-checkbox">
+								<input type="checkbox" id="lg_remember" name="lg_remember">
+								<label for="lg_remember">Remember Me</label>
+							</div>
+						</div>
+					</div>  	
+				</div>  
+			</div>	
+		</div>
+	</body>
+</html>
