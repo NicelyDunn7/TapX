5b3557b45939ef1e8e1ba0493927e4e15abb4500
ljhwc3 <ljhwc3@mail.missouri.edu>
2017-02-15 16:04:39 -0600

    Business Table Update
    I cannot seem to get the data correctly and dynamically creating the
    table

--- business/business-db.php
+++ business/business-db.php
@@ -10,13 +10,14 @@
 		while($row = mysqli_fetch_array($table_result)){
 			$response[$i]['table_id'] = $row['table_id'];
       $response[$i]['table_num'] = $row['table_num'];
-      $data['posts'][$i] = $response[$i]
+      $data['posts'][$i] = $response[$i];
       $i=$i+1;
 		}
 		$json_string =json_encode($data['posts']);
-    $file = 'bootstrap_file.json';
-    $file_put_contents($file, $json_string);
-    @header('Location: business-tables.php');
+    echo $json_string;
+    //$file = 'business-tables.php';
+    //$file_put_contents($file, $json_string);
+    //@header('Location: business-tables.php');
 
 		mysqli_close($conn);
 ?>
--- business/business-tables.php
+++ business/business-tables.php
@@ -2,28 +2,39 @@
 <html>
 	<head>
 		<title>Restaurant Name</title>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.mis.js"></script>
+		<!--script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.mis.js"></script-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<meta charset="utf-8">
  		<meta http-equiv="X-UA-Compatible" content="IE=edge">
  		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<script>
-			($function(){
-				$('#tables-javascript').bootstrapTable({
-					method: 'get',
-					url: 'bootstrap_file.json',
-					columns: [{
-						field: 'table_id',
-						title: 'table_id',
-						align: 'center'
-					},{
-						field: 'table_num',
-						title: 'table_num',
-						align: 'center'
-					}
-					}]
-				})
-			}
+		function createTable(key,value){
+			$.each(data, function(){
+				alert(key + ": " + value);
+				value++;
+			)};
+		},
+			$(document).ready(function(createTable){
+				$('.business-tables').click(function(event){
+					$.ajax({
+						type: 'get',
+						url: 'business-db.php',
+						data: $(this).val('id'),
+						success: function(data){
+							var data = $.parseJSON(data);
+							console.log(data);
+							alert(data);
+							createTable(data);
+						},
+						error: function(){
+								console.log("error");
+										alert("Error");
+						}
+						});
+					});
+
+			});
 		</script>
 
 		<style type="text/css">
@@ -46,7 +57,9 @@
 		<div class="container">
 			<div class="col-xs-12">
               <!-- My code starts here. Delete this comment after styling. -->
-				<button id='tables-javascript'>button</button>
+
+				<input id="test" type="button" class="business-tables">Button</input>
+				<button class='business-tables' id="number"></button>
 
 </script>
 
