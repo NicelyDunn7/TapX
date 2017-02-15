832c4a84ccfce9986b98e0c4413a60421b5b3968
ljhwc3 <ljhwc3@mail.missouri.edu>
2017-02-15 16:06:52 -0600

    Update
    Same thing as previous commit. Had to pull the funtion out of the script

--- business/business-tables.php
+++ business/business-tables.php
@@ -8,13 +8,15 @@
  		<meta http-equiv="X-UA-Compatible" content="IE=edge">
  		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<script>
+		<?php
 		function createTable(key,value){
 			$.each(data, function(){
 				alert(key + ": " + value);
 				value++;
 			)};
-		},
+		?>
+		<script>
+
 			$(document).ready(function(createTable){
 				$('.business-tables').click(function(event){
 					$.ajax({
