c9f873a05a2c0a25ff4d294a1aba07a83c1afcb3
dpdunn10 <dpdunn10@users.noreply.github.com>
2017-02-21 13:09:00 -0600

    Delete business-validate.php

--- business-validate.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(isset($_POST)) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $remember = $_POST['lg_remember'];
-    }else{
-        header("location: business-login.php")
-    }
-?>
