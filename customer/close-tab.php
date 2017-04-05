c22b0416b8de75c7d010599cc27926ecec2a8495
EC2 Default User <ec2-user@ip-172-31-48-129.ec2.internal>
2017-04-05 01:48:30 +0000

    added deleting cookie functionality on closetab.php, still need to send websocket of tab to bar

--- customer/close-tab.php
+++ customer/close-tab.php
@@ -2,4 +2,30 @@
 	if(!isset($_COOKIE['business_id']) || !isset($_COOKIE['table_id']) || !isset($_COOKIE['user_name'])){
 		header('Location: home.php');
 	}
+	include '../dbcreds.php';
+	
+	
+	
+	echo "gadafahihihihihih";
+	
+	if (isset($_COOKIE['business_id'])) {
+		setcookie('business_id', "", time()-3600);
+		setcookie('table_id', "", time()-3600);
+		setcookie('tab', "", time()-3600, '/');
+		setcookie('tab_price', "", time()-3600, '/');
+		setcookie('user_name', "", time()-3600);
+	}
+/*	
+	if(isset($_COOKIE['tab'])) {	///TAB IS LINKED TO QUANTITY OF DRINK
+		foreach (json_decode($_COOKIE['tab']) as $name => $quantity) {
+			setcookie('tab', "", time()-3600);
+		}
+	}
+	if(isset($_COOKIE['tab_price'])) {	///TAB PRICE IS PRICE OF QUANTITY OF DRINKS
+		foreach (json_decode($_COOKIE['tab_price']) as $name => $price) {
+			setcookie('tab_price', "", time()-3600, '/');
+		}
+	}
+*/	
+	header('Location: cust-order-form.php');
  ?>
