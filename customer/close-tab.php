e4e35e4d5d498665dcf324cdf7eccdb018a0a4c3
dpdunn10 <dpdunn10@gmail.com>
2017-04-06 15:32:14 -0500

    Update close-tab.php
    Don’t send websocket message to business if no order was every placed
    and the tab is empty

--- customer/close-tab.php
+++ customer/close-tab.php
@@ -2,34 +2,6 @@
 	if(!isset($_COOKIE['business_id']) || !isset($_COOKIE['table_id']) || !isset($_COOKIE['user_name'])){
 		header('Location: home.php');
 	}
-	include '../dbcreds.php';
-	echo "
-		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
-		<script language='javascript' type='text/javascript'>
-			$(document).ready(function(){
-				//Create new websocket
-				var addr = 'ws://tapx.duckdns.org:9998/websocket.php';
-				var ws = new WebSocket(addr);
-				//Open connection, send message to notify server a customer has connected
-				ws.onopen = function(ev) { // connection is open
-					var close = {
-						business_id: " . $_COOKIE['business_id'] . ",
-						type: \"close\",
-						name: \"" . $_COOKIE['user_name'] . "\",
-						table_id: " .  $_COOKIE['table_id'] . ",
-						quantity: \"NULL\",
-						item: \"NULL\"
-					};
-					ws.send(JSON.stringify(close));
-					if(window.console) console.log('Connected to Server.');
-					setTimeout(function(){
-						window.location.assign('home.php');
-					}, 2000);
-				}
-			});
-	    </script>
-
-	";
 
 	if (isset($_COOKIE['business_id'])) {
 		setcookie('business_id', "", time()-3600, "/");
@@ -39,5 +11,32 @@
 		setcookie('user_name', "", time()-3600, "/");
 	}
 
-	//header('Location: cust-order-form.php');
+	if(isset($_COOKIE['tab'])){
+		echo "
+			<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+			<script language='javascript' type='text/javascript'>
+				$(document).ready(function(){
+					//Create new websocket
+					var addr = 'ws://tapx.duckdns.org:9998/websocket.php';
+					var ws = new WebSocket(addr);
+					//Open connection, send message to notify server a customer has connected
+					ws.onopen = function(ev) { // connection is open
+						var close = {
+							business_id: " . $_COOKIE['business_id'] . ",
+							type: \"close\",
+							name: \"" . $_COOKIE['user_name'] . "\",
+							table_id: " .  $_COOKIE['table_id'] . ",
+							quantity: \"NULL\",
+							item: \"NULL\"
+						};
+						ws.send(JSON.stringify(close));
+						if(window.console) console.log('Connected to Server.');
+						setTimeout(function(){
+							window.location.assign('home.php');
+						}, 2000);
+					}
+				});
+		    </script>
+		";
+	}
  ?>
